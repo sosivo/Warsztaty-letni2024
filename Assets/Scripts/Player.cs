bf5fdbed7d7ca5480a12476211c52ec4285f5a4c
Monczak <37554206+Monczak@users.noreply.github.com>
2024-03-25 23:07:46 +0100

    Improve player controls

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,14 +6,33 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public Rigidbody2D Rb;
-    Rigidbody2D _heldRb;
+    private Rigidbody2D _heldRb;
+
+    private Collider2D col;
+    [SerializeField] private float groundCheckDistance = 0.1f;
+
+    private readonly RaycastHit2D[] groundHitCheckResults = new RaycastHit2D[10];
+
+    [SerializeField] private float movementSpeed = 4f;
+    [SerializeField] private float jumpForce = 8f;
+
+    [SerializeField] private float horizontalVelocityLimit = 5f;
+    [SerializeField] private float horizontalDrag = 40f;
+
+    [SerializeField] private float jumpHeldGravityScale = 0.8f;
+    [SerializeField] private float jumpReleasedGravityScale = 1.6f;
+
+    [SerializeField] private float jumpBuffer = 0.2f;
+    private bool hasJumped;
+    private float lastJumpTime;
+    
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         //Fool proof for the sake of flex xd
         if (Rb == null)
         {
-            if (TryGetComponent<Rigidbody2D>(out Rigidbody2D rb))
+            if (TryGetComponent(out Rigidbody2D rb))
             {
                 Rb = rb;
             }
@@ -22,25 +41,42 @@ public class Player : MonoBehaviour
                 Rb = transform.AddComponent<Rigidbody2D>();
             }
         }
+
+        col = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        const float MOVEMENT_SCALLAR = 4f;
-        const float JUMP_SCALLAR = 8f;
-        Vector2 desiredVelocity = Vector2.zero;
+        if (Input.GetKeyDown(KeyCode.W))
+            lastJumpTime = Time.time;
+            
+        if (CanJump())
+        {
+            if (IsGrounded())
+            {
+                Rb.velocity = new Vector2(Rb.velocity.x, 0);
+                Rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+                hasJumped = true;
+            }
+        }
+            
 
-        //allows MultiJump for testing
-        if(Input.GetKeyDown(KeyCode.W))
-            desiredVelocity += Vector2.up * JUMP_SCALLAR;
+        if (Rb.velocity.y < -Mathf.Epsilon) hasJumped = false;
+
+        Rb.gravityScale = (Input.GetKey(KeyCode.W), Rb.velocity.y > Mathf.Epsilon) switch
+        {
+            (true, true) => jumpHeldGravityScale,
+            (false, true) => jumpReleasedGravityScale,
+            _ => 1
+        };
+
         if (Input.GetKey(KeyCode.A))
-            desiredVelocity += Vector2.left * MOVEMENT_SCALLAR;
+            Rb.AddForce(Vector2.left * movementSpeed, ForceMode2D.Force);
         if (Input.GetKey(KeyCode.D))
-            desiredVelocity += Vector2.right * MOVEMENT_SCALLAR;
-        Rb.velocity = desiredVelocity + new Vector2(0f,Rb.velocity.y);
+            Rb.AddForce(Vector2.right * movementSpeed, ForceMode2D.Force);
 
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             if (_heldRb == null)
                 Interact();
@@ -53,10 +89,15 @@ public class Player : MonoBehaviour
     }
     private void FixedUpdate()
     {
+        Rb.velocity = new Vector2(Mathf.Clamp(Rb.velocity.x, -horizontalVelocityLimit, horizontalVelocityLimit) * horizontalDrag * Time.fixedDeltaTime,
+            Rb.velocity.y);
+        
         HeldItemPhysics();
     }
 
-    void Interact()
+    private bool CanJump() => Input.GetKey(KeyCode.W) && Time.time - lastJumpTime < jumpBuffer && !hasJumped;
+
+    private void Interact()
     {
         RaycastHit2D[] hits = Physics2D.BoxCastAll(transform.position, new Vector2(2f, 2f), 0f, Vector2.zero,0f);
         foreach(RaycastHit2D hit in hits)
@@ -79,17 +120,21 @@ public class Player : MonoBehaviour
         Rb.mass += _heldRb.mass;
         _heldRb.simulated = false;
     }
-    public void DropItem()
+
+    private void DropItem()
     {
         Rb.mass -= _heldRb.mass;
         _heldRb.simulated = true;
         _heldRb.velocity = Vector3.zero;
         _heldRb = null;
     }
-    public void HeldItemPhysics()
+
+    private void HeldItemPhysics()
     {
         if (_heldRb == null)
             return;
         _heldRb.transform.position = transform.position + Vector3.up * 1.5f;
     }
+
+    private bool IsGrounded() => col.Cast(Vector2.down, groundHitCheckResults, groundCheckDistance) > 0;
 }
